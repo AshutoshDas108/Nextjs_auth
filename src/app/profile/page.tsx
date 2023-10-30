9fa88b4d74b10043c8e210b0b11fca44a81d1901
AshutoshDas <ashutosh.rkl.2001@gmail.com>
2023-10-30 13:04:58 +0530

    raw Logout logic

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,8 +1,33 @@
+"use client";
+import axios from "axios";
+import Link from "next/link";
+import React, {useState} from "react";
+import {toast} from "react-hot-toast";
+import {useRouter} from "next/navigation";
+
 export default function Profile(){
+    const router = useRouter();
+    const  logout = async () => {
+        try {
+            await axios.get('/api/user/logout')
+            toast.success('Logout successful')
+            router.push('/login')
+        } catch (error: any) {
+            console.log(error.message);
+            toast.error(error.message)
+        }
+    }
+
+
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1 className="text-center text-black bg-orange-600 rounded-md">
                 Profile Page</h1>
+               <br/>
+                <button onClick={logout}
+                className=" text-xl text-center text-white bg-blue-600 rounded-md">
+                    Logout
+                </button>
                 </div>
     )
 }
